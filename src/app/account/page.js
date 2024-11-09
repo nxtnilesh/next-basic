5c61bbeb790b0c80c177d75261d0869e032debd5
nxtnilesh <nxtnilesh@gmail.com>
2024-11-09 11:49:50 +0530

    use of redirect in server side in account page

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -1,3 +1,6 @@
+import { redirect } from "next/navigation";
 export default function Account() {
+  const userProfile = "";
+  if (!userProfile) redirect("profile");
   return <div>Account Page</div>;
 }
