10836bbb3a84ae461040b0497bbeb7c414c764c1
nxtnilesh <nxtnilesh@gmail.com>
2024-11-09 11:21:23 +0530

    nested routing

---
+++ src/app/products/[details]/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Details() {
+  return (
+    <div>
+      Products Details
+    </div>
+  )
+}
+
+export default Details
---
+++ src/app/products/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      Products page
+    </div>
+  )
+}
+
+export default page
